d73a7d39a4ee832204cca019e2f09be3853d3b48
Mikio Ikoma <mikio.ikoma@gmail.com>
2016-08-10 05:32:01 +0900

    Regexp::samples/sample/match_data support options

--- lib/regtest/regexp.rb
+++ lib/regtest/regexp.rb
@@ -1,31 +1,27 @@
 class Regexp
 
   # generate sample strings of regexp
-  def samples(num=1)
+  def samples(num = 1, option = {})
     if num == 1
-      sample
+      sample(option)
     else
-      match_data(num).map{|elem| elem.string}
+      match_data(num, option).map{|elem| elem.string}
     end
   end
   
   # generate a sample string of regexp
-  def sample
+  def sample(option = {})
     match_data.string
   end
   
   # generate match-data object(s) of regexp
-  def match_data(num=1)
-    regtest = Regtest.new(self)
+  def match_data(num = 1, option = {})
+    regtest = Regtest.new(self, option)
     if num == 1
       result = regtest.generate
     else
-      result = []
-      num.times do
-        result.push regtest.generate
-      end
+      result = num.times.inject([]){|array| array.push regtest.generate}
     end
-    result
   end
   
 end
