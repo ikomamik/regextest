2246b4421777f2fe9005045e743aa950a4ebced5
Mikio Ikoma <mikio.ikoma@gmail.com>
2016-08-31 20:44:46 +0900

    ignores "systax error" case for generating test suite

--- lib/pre-generate-spec.rb
+++ lib/pre-generate-spec.rb
@@ -24,11 +24,11 @@ class Regextest::Test
         eval(line)
       rescue SyntaxError => ex
         warn "EVAL ERROR: #{ex}, #{line}"
-        results.push({
-          explain: "raises #{ex}, when eval (#{line})",
-          expect:  "eval(#{line})",
-          raises:   "#{ex.class}"
-        })
+        #results.push({
+        #  explain: "raises #{ex}, when eval (#{line})",
+        #  expect:  "eval('#{line}')",
+        #  raises:   "#{ex.class}"
+        #})
       end
     end
     results
@@ -39,11 +39,13 @@ class Regextest::Test
       fp.puts "require 'spec_helper'"
       fp.puts "describe Regextest do"
       results.each do | result |
-        fp.puts "  it '#{result[:explain]}' do"
+        explain = result[:explain].gsub("'", "\\\\'")
+        expect = result[:expect]   # .gsub("'", "\\\\'")
+        fp.puts "  it '#{explain}' do"
         if result[:to_be]
-          fp.puts "    expect(#{result[:expect]}).to be_a(#{result[:to_be]})"
+          fp.puts "    expect(#{expect}).to be_a(#{result[:to_be]})"
         elsif result[:raises]
-          fp.puts "    expect {#{result[:expect]}}.to raise_error(#{result[:raises]})"
+          fp.puts "    expect {#{expect}}.to raise_error(#{result[:raises]})"
         else
           raise "Invalid result: #{result}"
         end
@@ -58,7 +60,7 @@ class Regextest::Test
     # py_source = IO.read("./contrib/Onigmo/testpy.py")
     File::open("./contrib/Onigmo/testpy.py") do |f|
       f.each.with_index do |line, line_num|
-        break if line_num > 400
+        # break if line_num > 800
         line.force_encoding("utf-8")
         if !line.match(/ONIG_SYNTAX_PERL/)
           if(md = line.match(/^\s*(?:x|x2|n)\s*\(.+?$/u) rescue nil)
@@ -75,7 +77,7 @@ class Regextest::Test
 
   def check_normal_test(reg, results, timeout_seconds)
     result = nil
-    a_test = /#{reg}/
+    a_test = eval("/#{reg}/")
     timeout(timeout_seconds) do
       10.times do | i |
         md = a_test.match_data(seed: i)
@@ -99,7 +101,7 @@ class Regextest::Test
     end
     results.push result
   rescue => ex
-    warn "#{ex}"
+    warn "GOO: #{ex} #{ex.class}"
     results.push({
       explain: "raises #{ex}, when parse / generate (#{reg})",
       expect:  "/#{reg}/.match_data",
